44e4af1debf72386aeff9b46c66394e6dd97e098
hakim sedoud <hakimsedoud@MacBook-Pro-de-hakim.local>
2023-05-25 19:14:30 +0200

    multi-Upload-Pics11

--- routes/offer.js
+++ routes/offer.js
@@ -35,7 +35,7 @@ const fileUpload = multer();
 router.post(
   "/offer/publish",
   isAuthenticated,
-  fileUpload.array("picture", 5),
+  fileUpload.array("file", 5),
   async (req, res) => {
     try {
       const reqBody = req.body;
